ab09b10848151a50e3302a408163b71be572b630
Dhruv Gupta <ddgupta_b24@et.vjti.ac.in>
2026-02-09 19:08:14 +0530

    Add query method to fetch all issue languages

--- src/main/java/com/github/regyl/gfi/repository/DataRepository.java
+++ src/main/java/com/github/regyl/gfi/repository/DataRepository.java
@@ -4,6 +4,7 @@ import com.github.regyl.gfi.controller.dto.request.DataRequestDto;
 import com.github.regyl.gfi.controller.dto.response.IssueResponseDto;
 import com.github.regyl.gfi.controller.dto.response.LabelStatisticResponseDto;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.Collection;
 import java.util.List;
@@ -13,9 +14,20 @@ public interface DataRepository {
 
     List<IssueResponseDto> findAllIssues(DataRequestDto requestDto);
 
+    @Select("""
+        SELECT language
+        FROM gfi.issue_v
+        WHERE language IS NOT NULL
+        GROUP BY language
+        ORDER BY COUNT(1) DESC
+    """)
     Collection<String> findAllLanguages();
 
     String findRandomIssueLink(DataRequestDto filters);
 
     List<LabelStatisticResponseDto> findAllLabels();  
+
+    
+
+
 }

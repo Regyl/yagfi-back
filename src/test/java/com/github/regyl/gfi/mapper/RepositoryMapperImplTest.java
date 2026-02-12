e25acc442f6d5816d0171a720331413cc3264ae1
PedroAlex65 <pedroalex663@gmail.com>
2026-02-12 14:27:56 -0300

    style: fix checkstyle violations

--- src/test/java/com/github/regyl/gfi/mapper/RepositoryMapperImplTest.java
+++ src/test/java/com/github/regyl/gfi/mapper/RepositoryMapperImplTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.OffsetDateTime;
@@ -18,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class RepositoryMapperImplTest {
+ class RepositoryMapperImplTest {
     @InjectMocks
     private RepositoryMapperImpl repositoryMapper;
 

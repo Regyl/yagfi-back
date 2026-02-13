46c1f302c6c9a028630af4be47a47e0c0f3c895f
PedroAlex65 <pedroalex663@gmail.com>
2026-02-12 21:40:16 -0300

    style: fix import ordering for checkstyle

--- src/test/java/com/github/regyl/gfi/service/impl/feed/cyclonedx/homepage/github/GithubPurlToHomepageServiceImplTest.java
+++ src/test/java/com/github/regyl/gfi/service/impl/feed/cyclonedx/homepage/github/GithubPurlToHomepageServiceImplTest.java
@@ -1,9 +1,9 @@
 package com.github.regyl.gfi.service.impl.feed.cyclonedx.homepage.github;
 
-
 import com.github.packageurl.PackageURL;
 import com.github.regyl.gfi.annotation.DefaultUnitTest;
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DefaultUnitTest

7f0bce284533c30b1330212cebc7a18a14814bc3
Suganthi Thomas <sugan.mercy@gmail.com>
2026-02-17 14:38:20 -0600

    Add retry handling and recover logic

--- src/main/java/com/github/regyl/gfi/service/impl/GithubRetryService.java
+++ src/main/java/com/github/regyl/gfi/service/impl/GithubRetryService.java
@@ -1,12 +1,10 @@
 package com.github.regyl.gfi.service.impl;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.resilience.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpServerErrorException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import com.github.regyl.gfi.controller.dto.github.issue.IssueDataDto;
 import com.github.regyl.gfi.controller.dto.request.issue.IssueRequestDto;
 import com.github.regyl.gfi.service.github.GithubClientService;
@@ -15,7 +13,6 @@ import com.github.regyl.gfi.service.github.GithubClientService;
 @Service
 @RequiredArgsConstructor
 public class GithubRetryService {
-	// private static int counter = 0;
 
 	private final GithubClientService<IssueRequestDto, IssueDataDto> githubClient;
 
@@ -23,15 +20,6 @@ public class GithubRetryService {
 			HttpServerErrorException.BadGateway.class }, maxRetries = 4, delayString = "500ms", multiplier = 1.5)
 	public IssueDataDto fetchWithRetry(IssueRequestDto task) {
 
-		/*
-		 * counter++;
-		 * 
-		 * if (counter < 3) { log.info("Simulating BadGateway attempt {}", counter);
-		 * throw HttpServerErrorException.create( HttpStatus.BAD_GATEWAY,
-		 * "Simulated 502", null, null, null ); }
-		 */
-
-		// log.info("Success on attempt {}", counter);
 		log.info("Fetching issues for query: {}", task.getQuery());
 		return githubClient.execute(task);
 	}

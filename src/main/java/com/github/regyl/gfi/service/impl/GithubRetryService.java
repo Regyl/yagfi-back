837d2b0ea24ee43992d6c50ea3ee88f39fceed05
Suganthi Thomas <sugan.mercy@gmail.com>
2026-02-17 15:05:42 -0600

    Bug 85 code review implmentation

--- src/main/java/com/github/regyl/gfi/service/impl/GithubRetryService.java
+++ src/main/java/com/github/regyl/gfi/service/impl/GithubRetryService.java
@@ -3,8 +3,10 @@ package com.github.regyl.gfi.service.impl;
 import org.springframework.resilience.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpServerErrorException;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import com.github.regyl.gfi.controller.dto.github.issue.IssueDataDto;
 import com.github.regyl.gfi.controller.dto.request.issue.IssueRequestDto;
 import com.github.regyl.gfi.service.github.GithubClientService;
@@ -14,14 +16,17 @@ import com.github.regyl.gfi.service.github.GithubClientService;
 @RequiredArgsConstructor
 public class GithubRetryService {
 
-	private final GithubClientService<IssueRequestDto, IssueDataDto> githubClient;
+    private final GithubClientService<IssueRequestDto, IssueDataDto> githubClient;
 
-	@Retryable(includes = {
-			HttpServerErrorException.BadGateway.class }, maxRetries = 4, delayString = "500ms", multiplier = 1.5)
-	public IssueDataDto fetchWithRetry(IssueRequestDto task) {
-
-		log.info("Fetching issues for query: {}", task.getQuery());
-		return githubClient.execute(task);
-	}
+    @Retryable(
+        includes = { HttpServerErrorException.BadGateway.class },
+        maxRetries = 4,
+        delayString = "500ms",
+        multiplier = 1.5
+    )
+    public IssueDataDto fetchWithRetry(IssueRequestDto task) {
+        log.info("Fetching issues for query: {}", task.getQuery());
+        return githubClient.execute(task);
+    }
 
 }

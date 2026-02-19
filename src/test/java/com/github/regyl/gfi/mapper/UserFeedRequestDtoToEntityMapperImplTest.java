1f9498986d972db137f5a41d8fdf59eaa5135f43
Luiz Felipe <vluizinho2@gmail.com>
2026-02-18 23:39:07 -0300

    Add null handling and unit tests for UserFeedRequestDtoToEntityMapper

---
+++ src/test/java/com/github/regyl/gfi/mapper/UserFeedRequestDtoToEntityMapperImplTest.java
@@ -0,0 +1,10 @@
+package com.github.regyl.gfi.mapper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserFeedRequestDtoToEntityMapperImplTest {
+
+    @org.junit.jupiter.api.Test
+    void apply() {
+    }
+}

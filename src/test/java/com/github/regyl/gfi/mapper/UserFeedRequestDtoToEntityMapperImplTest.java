786de68cc2c60c5d0dd55582aa4deb696b79bd51
anujwagh09 <anuj_wagh_comp@moderncoe.edu.in>
2026-03-20 12:34:12 +0530

    Fix checkstyle formatting issues

--- src/test/java/com/github/regyl/gfi/mapper/UserFeedRequestDtoToEntityMapperImplTest.java
+++ src/test/java/com/github/regyl/gfi/mapper/UserFeedRequestDtoToEntityMapperImplTest.java
@@ -1,15 +1,15 @@
 package com.github.regyl.gfi.mapper;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import java.time.OffsetDateTime;
 import java.util.function.Supplier;
 
 import org.junit.jupiter.api.Test;
 
 import com.github.regyl.gfi.dto.request.feed.UserFeedRequestDto;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class UserFeedRequestDtoToEntityMapperImplTest {
 
